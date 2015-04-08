311099479efd88a353a2240c0dcb26daeaf062ba
Pomin Wu <pomin5@gmail.com>
2015-04-08 14:43:14 +0800

    add test details command

--- app.js
+++ app.js
@@ -24,7 +24,11 @@ var argv = process.argv.slice(2);
 if (argv.length === 0) {
   console.log("Usage: sccli [command]");
   console.log("Commands:");
-  console.log("  test     list all tests");
+  console.log("  test               list all tests");
+  console.log("  test <id>          show details of a test");
+  console.log("  test add           add a test");
+  console.log("  test remove <id>   remove a test");
+  console.log("  test edit <id>     edit a test");
   process.exit(0);
 }
 
@@ -37,6 +41,16 @@ if (argv[0] === "test" && argv.length === 1) {
       table.push([
         d.TestID, d.WebsiteName, d.Status, d.Uptime
       ]);
+    });
+    console.log(table.toString());
+  });
+} else if (argv[0] === "test" && argv.length === 2) {
+  statuscake.testsDetails(parseInt(argv[1]), function (err, data) {
+    var table = new cliTable();
+    ["TestID", "WebsiteName", "URI", "Method", "Uptime", "DownTimes", "LastTested", "ContactGroup"].forEach(function (name) {
+      var d = {};
+      d[name] = data[name] || "";
+      table.push(d);
     });
     console.log(table.toString());
   });
