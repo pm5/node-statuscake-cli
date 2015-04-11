d60ae27715b762facbfa9864d18ce8587592cdf8
Pomin Wu <pomin5@gmail.com>
2015-04-11 23:34:09 +0800

    refine test update command

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var yargs = require('yargs')
   .command('test <id>', 'show details of a test')
   .command('test add', 'add a test')
   .command('test remove <id>', 'remove a test')
-  .command('test edit <id>', 'edit a test')
+  .command('test update <id>', 'update a test')
   .demand(1, 'Error: must provide a valid command.'),
   argv = yargs.argv,
   cmd = argv._[0],
@@ -34,11 +34,12 @@ var cliTable = require("cli-table");
 if (cmd === "test" && subcmd === undefined) {
   statuscake.tests(function (err, data) {
     var table = new cliTable({
-      head: ["TestID", "WebsiteName", "Status", "Uptime"],
+      head: ["TestID", "WebsiteName", "Status", "Uptime", "ContactGroup"],
     });
     data.forEach(function (d) {
+      console.log(d);
       table.push([
-        d.TestID, d.WebsiteName, d.Status, d.Uptime
+        d.TestID, d.WebsiteName, d.Status, d.Uptime, (d.ContactGroup || '') + ' (' + d.ContactID + ')'
       ]);
     });
     console.log(table.toString());
@@ -46,7 +47,7 @@ if (cmd === "test" && subcmd === undefined) {
 } else if (cmd === "test" && ! isNaN(parseInt(subcmd))) {
   statuscake.testsDetails(parseInt(subcmd), function (err, data) {
     var table = new cliTable();
-    ["TestID", "WebsiteName", "URI", "Method", "CheckRate", "Uptime", "DownTimes", "LastTested", "TestType", "ContactGroup"].forEach(function (name) {
+    ["TestID", "WebsiteName", "URI", "Method", "CheckRate", "Uptime", "DownTimes", "LastTested", "TestType", "ContactGroup", "TestTags"].forEach(function (name) {
       var d = {};
       d[name] = data[name] || "";
       table.push(d);
@@ -61,17 +62,20 @@ if (cmd === "test" && subcmd === undefined) {
     TestType:     argv["test-type"] || 'HTTP',
   };
   if (subcmd === "update") {
-    data.TestID === argv._[2];
+    data.TestID = argv._[2];
   }
   ["Paused", "Port", "NodeLocations", "Timeout", "PingURL", "Confirmation", "BasicUser", "BasicPass", "Public", "LogoImage", "Branding", "WebsiteHost", "Virus", "FindString", "DoNotFind", "ContactGroup", "RealBrowser", "TriggerRate", "TestTags"].forEach(function (field) {
-    data[field] = argv[field.replace(/^([A-Z]+)/, "--$1").replace(/^([A-Z]+)/g, "-$1").toLowerCase()];
+    var argName = field[0].toLowerCase().concat(field.substr(1));
+    if (argv[argName] !== undefined) {
+      data[field] = argv[argName];
+    }
   });
   statuscake.testsUpdate(data, function (err, res) {
     if (res.Success) {
       if (subcmd === "add") {
         console.log("Test added with ID " + res.InsertID + ".");
       } else {
-        console.log("Test with ID " + res.InsertID + " updated.");
+        console.log("Test with ID " + data.TestID + " updated.");
       }
       return;
     }
